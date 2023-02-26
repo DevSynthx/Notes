c4fe6cd4a867cedc1106f2a2a00335bdd5bd2a54
DavidEtoedia <etoedia@gmail.com>
2023-02-26 15:21:19 -0800

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Inyene Etoedia on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
